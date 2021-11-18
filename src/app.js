3215cae76a40047c3ba77f441762013a06920b6c
serikanbey <76795901+serikanbey@users.noreply.github.com>
2021-11-18 14:15:59 +0300

    18 Ekim Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,27 @@
 console.log("Merhaba Kodlama.io")
 
-//JS type safe değildir
-let dolarBugun = 9.30
 
-let dolarDun = 9.20
-dolarDun = "9.20"
+let dolarBugun=9.30
+
 {
-    let dolarDun = 9.10
+    let dolarDun=9.20
+console.log(dolarDun)
 }
 
-console.log(dolarDun)
 
-const euroDun = 11.2
-//euroDun = 11
 
+const euroDun=11.2
+// euroDun=11 HATA OLUR CUNKU CONST ATANDI, SABİT DEĞER ÜZERİNE DEĞER ATAMAYIZ
 console.log(euroDun)
 
 //array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+ let konutKredileri=["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
 
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
+for (let i = 0; i < konutKredileri.length; i++) {
     console.log("<li>"+konutKredileri[i]+"</li>")
+    
 }
 console.log("</ul>")
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
+console.log(konutKredileri) 
